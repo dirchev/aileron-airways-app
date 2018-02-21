2e95da0252ba71a3be3fe3443d9397288adad601
dirchev <dirchev@camplight.net>
2018-02-21 23:43:43 +0000

    update App component
    execute router logic instead of showing content
    
    the router currently has 3 pages:
    
    * HomePage
    * TimelinePage
    * EventPage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import TimelinePage from './pages/TimelinePage';
+import EventPage from './pages/EventPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="hero">
-        <div className="hero-body">
-          <div className="title">
-            <h1>Aileron Airways</h1>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={HomePage}></Route>
+            <Route exact path="/timeline" component={TimelinePage}></Route>
+            <Route exact path="/event" component={EventPage}></Route>
           </div>
-          <div className="subtitle">
-            IP3 Project
-          </div>
-        </div>
+        </BrowserRouter>
       </div>
     )
   }

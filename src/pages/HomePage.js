3a0337e60b7b7dc9cc99483aa7a446e66e86eda8
dirchev <dirchev@camplight.net>
2018-02-23 00:24:37 +0000

    update pages to use the new navigation components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
+import Navigation from '../components/Navigation'
+import TimelineSearchInput from '../components/nav-items/TimelineSearchInput'
+import TimelineCreateButton from '../components/nav-items/CreateTimelineButton'
 
 class HomePage extends Component {
+  getNavigationItems () {
+    return {
+      actionsRight: [<TimelineSearchInput />, <TimelineCreateButton />]
+    }
+  }
+
   render() {
     return (
-      <div className="hero">
-        <div className="hero-body">
-          <div className="title">
-            <h1>Aileron Airways</h1>
-          </div>
-          <div className="subtitle">
-            IP3 Project - Home Page
+      <div>
+        <Navigation {...this.getNavigationItems()}/>
+        <div className="hero">
+          <div className="hero-body">
+            <div className="title">
+              <h1>Aileron Airways</h1>
+            </div>
+            <div className="subtitle">
+              IP3 Project - Home Page
+            </div>
           </div>
         </div>
       </div>

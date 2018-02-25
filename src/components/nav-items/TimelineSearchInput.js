86ec9022936d07c10cea3a5b9702e49bc304baed
dirchev <dirchev@camplight.net>
2018-02-25 03:01:31 +0000

    Update on TimelineSearchInput
    * connect to store
    * add search term handler
    * dispatch appropriate action on search term change

--- src/components/nav-items/TimelineSearchInput.js
+++ src/components/nav-items/TimelineSearchInput.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+import timelineActions from '../../actions/timeline'
+import { connect } from 'react-redux'
+import _ from 'lodash'
 
 class TimelineSearchInput extends Component {
+  constructor () {
+    super()
+    this.onFilterChange = this.onFilterChange.bind(this)
+  }
+
+  onFilterChange (value) {
+    this.props.setTimelinesFilter(value)
+  }
+
   render() {
     return (
       <div className="navbar-item" key="search-input">
         <div className="field">
           <p className="control has-icons-right">
-            <input className="input" name="search" placeholder="Search event..." type="text" />
+            <input
+              className="input"
+              name="search"
+              placeholder="Search timeline..."
+              type="text"
+              onKeyUp={({target}) => _.debounce(this.onFilterChange, 300)(target.value)}
+            />
             <span className="icon is-small is-right">
               <i className="fa fa-search"></i>
             </span>
@@ -17,4 +35,8 @@ class TimelineSearchInput extends Component {
   }
 }
 
-export default TimelineSearchInput
+const mapDispatchToProps = {
+  setTimelinesFilter: timelineActions.setTimelinesFilter
+}
+
+export default connect(null, mapDispatchToProps)(TimelineSearchInput)

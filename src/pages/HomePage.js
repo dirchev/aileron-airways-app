13793261a6432c698af235b6b000777734fc447c
dirchev <dirchev@camplight.net>
2018-02-25 03:00:03 +0000

    HomePage update - timelines fetch and search
    * fetch all timelines on load
    * parse timelines object from store
    * filter timelines based on search term
    * render timelines list

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
 import Navigation from '../components/Navigation'
+import TimelinesList from '../components/TimelinesList'
 import TimelineSearchInput from '../components/nav-items/TimelineSearchInput'
 import TimelineCreateButton from '../components/nav-items/CreateTimelineButton'
 
+import timelineActions from '../actions/timeline'
+import { connect } from 'react-redux'
+import _ from 'lodash'
+
 class HomePage extends Component {
+  getTimelinesArray () {
+    var result = _.chain(this.props.timelines).values()
+    if (this.props.timelinesFilter) {
+      result = result.filter((timeline) => {
+        return timeline.Title.toLowerCase().indexOf(this.props.timelinesFilter) !== -1
+      })
+    }
+
+    result = result.sortBy('CreatedTimeStamp').reverse()
+
+    return result.value()
+  }
+
   getNavigationItems () {
     return {
       actionsRight: [
@@ -13,23 +31,42 @@ class HomePage extends Component {
     }
   }
 
+  componentDidMount () {
+    this.props.fetchAll()
+  }
+
   render() {
     return (
       <div>
         <Navigation {...this.getNavigationItems()}/>
-        <div className="hero">
-          <div className="hero-body">
-            <div className="title">
-              <h1>Aileron Airways</h1>
-            </div>
-            <div className="subtitle">
-              IP3 Project - Home Page
+        <div className="container is-fluid">
+          <div className="hero">
+            <div className="hero-body">
+              <div className="title">
+                <h1>Timelines</h1>
+              </div>
+              <div className="subtitle">
+                <p>{this.getTimelinesArray().length} timelines available</p>
+              </div>
             </div>
           </div>
+          <TimelinesList timelines={this.getTimelinesArray()}/>
         </div>
       </div>
     )
   }
 }
 
-export default HomePage
+const mapStateToProps = (state, ownProps) => {
+  return {
+    ...ownProps,
+    timelines: state.timelines,
+    timelinesFilter: state.ui.timelinesFilter,
+  }
+}
+
+const mapDispatchToProps = {
+  fetchAll: timelineActions.fetchAll
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)

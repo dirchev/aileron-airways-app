d67c651cef472cff543d7ca829bb57c1a919ae6b
dirchev <dirchev@camplight.net>
2018-02-25 03:03:06 +0000

    add redux store singleton

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import reducer from './reducers'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+)
+
+export default store

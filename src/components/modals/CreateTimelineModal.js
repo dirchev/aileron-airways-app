4829f7307e6003abaea7f5011bf27a4b34b98a9b
dirchev <dirchev@camplight.net>
2018-03-03 20:18:55 +0000

    implement timeline create modal

---
+++ src/components/modals/CreateTimelineModal.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import Input from '../inputs/Input'
+import timelineActions from '../../action-creators/timeline'
+
+class CreateTimelineModal extends Component {
+  constructor () {
+    super()
+    this.state = {
+      title: ''
+    }
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChange (value) {
+    this.setState({title: value})
+  }
+
+  onSubmit (e) {
+    e.preventDefault()
+    this.props.createTimeline({Title: this.state.title})
+    this.props.onClose()
+  }
+
+  render () {
+    return (
+      <div className="modal is-active">
+        <div className="modal-background"></div>
+        <div className="modal-content">
+          <div className="card">
+            <div className="card-header">
+              <div className="card-header-title">
+                <h2>Create Timeline</h2>
+              </div>
+            </div>
+            <div className="card-content">
+              <form onSubmit={this.onSubmit}>
+                <Input
+                  onChange={this.onChange}
+                  value={this.state.title}
+                  label="Title"
+                  placeholder="Please enter timeline title..."
+                />
+                <div className="field is-grouped">
+                  <div className="control">
+                    <button className="button is-text" type="button" onClick={this.props.onClose}>Cancel</button>
+                  </div>
+                  <div className="control">
+                    <button className="button is-link" type="submit">Submit</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createTimeline: (timelineData) => dispatch(timelineActions.create(timelineData))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CreateTimelineModal)
--- src/components/nav-items/CreateTimelineButton.js
+++ src/components/nav-items/CreateTimelineButton.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react'
-import timelineActions from '../../action-creators/timeline'
 import { connect } from 'react-redux'
 
-let count = 1
+import uiActions from '../../action-creators/ui'
 
 class CreateTimelineButton extends Component {
   constructor () {
     super()
-    this.showEditTimelineModal = this.showEditTimelineModal.bind(this)
-  }
-
-  showEditTimelineModal () {
-    this.props.createTimeline({
-      Title: 'New Timeline ' + count++
-    })
   }
 
   render() {
     return (
-      <a className="navbar-item" onClick={this.showEditTimelineModal} key="create-event-button">
+      <a className="navbar-item" onClick={this.props.openModal} key="create-event-button">
         <i className="fa fa-plus mr-sm"></i>
         Create Timeline
       </a>
@@ -28,8 +20,8 @@ class CreateTimelineButton extends Component {
 
 const mapDispatchToProps = function (dispatch) {
   return {
-    createTimeline: (timelineData) => {
-      dispatch(timelineActions.create(timelineData))
+    openModal: () => {
+      dispatch(uiActions.openModal('createTimeline'))
     }
   }
 }

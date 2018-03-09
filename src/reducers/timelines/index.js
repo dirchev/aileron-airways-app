9d2a80867c01ff327f5b18d89d5e79e2366e9dff
dirchev <dirchev@camplight.net>
2018-03-09 15:09:16 +0000

    add edit timeline title reducers

--- src/reducers/timelines/index.js
+++ src/reducers/timelines/index.js
@@ -1,16 +1,9 @@
 const defaultState = {}
 
-/*
-  Timeline Reducer
-  Contains reducers related to timelines
- */
 export default function timelinesReducer (state = defaultState, action) {
   switch(action.type) {
-    // For the given timeline:
-    // - set data
-    // - set synced to false
-    // - save loading to true
     case 'START_CREATE_TIMELINE':
+    case 'START_EDIT_TIMELINE':
       return {
         ...state,
         [action.data.Id]: {
@@ -20,11 +13,8 @@ export default function timelinesReducer (state = defaultState, action) {
         }
       }
 
-    // For the given timeline:
-    // - set add new data from server
-    // - set synced to true
-    // - save loading to false
     case 'SUCCESS_CREATE_TIMELINE':
+    case 'SUCCESS_EDIT_TIMELINE':
       return {
         ...state,
         [action.data.Id]: {
@@ -34,10 +24,7 @@ export default function timelinesReducer (state = defaultState, action) {
         }
       }
 
-    // For the given timeline:
-    // - set loading to false
-    // - set synced to false
-    // - save error message
+    case 'ERROR_EDIT_TIMELINE':
     case 'ERROR_CREATE_TIMELINE':
       return {
         ...state,
@@ -48,6 +35,7 @@ export default function timelinesReducer (state = defaultState, action) {
             error: action.error
         }
       }
+
     case 'SUCCESS_FETCH_TIMELINES':
       var newState = {}
       action.data.forEach(function (timeline) {

3406795491af756633103dae323f2b0f3c2cda09
dirchev <dirchev@camplight.net>
2018-03-12 11:01:41 +0000

    update event page
    - remove not needed navigation
    - show event data

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -1,32 +1,57 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import moment from 'moment'
+import { Link, Redirect } from 'react-router-dom'
+
 import Navigation from '../components/Navigation'
-import EditEventButton from '../components/nav-items/EditEventButton'
 
 class EventPage extends Component {
   getNavigationItems () {
     var backButton = (
-      <a className="navbar-item" href="" key="back-button">
+      <Link to={`/timeline/${this.props.event.TimelineId}`}
+        className="navbar-item"
+        href=""
+        key="back-button">
         <i className="fa fa-chevron-left mr-sm"></i>
         Back
-      </a>
+      </Link>
     )
 
     return {
       actionsLeft: [backButton],
-      actionsRight: [
-        <EditEventButton key="edit-event"/>
-      ],
     }
   }
 
   render() {
+    if (!this.props.event) {
+      return (<Redirect to="/"/>)
+    }
     return (
-      <div>
+      <div className="mb-lg">
         <Navigation {...this.getNavigationItems()}/>
-        <div className="hero">
-          <div className="hero-body">
-            <div className="title">
-              <h1>Event Page</h1>
+        <div className="container is-fluid">
+          <div className="columns mt-lg">
+            <div className="column is-one-quarter">
+              Linked Events Map Here
+            </div>
+            <div className="column">
+              <div className="card">
+                <div className="card-content">
+                  <div className="title">
+                    <h1>{this.props.event.Title}</h1>
+                  </div>
+                  {
+                    this.props.event.EventDateTime
+                    ? (
+                      <div className="subtitle">
+                        <time className="subtitle">{moment(this.props.event.EventDateTime).format("dddd, MMMM Do YYYY, h:mm:ss a")}</time>
+                      </div>
+                    )
+                    : null
+                  }
+                  <p>{this.props.event.Description}</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -35,4 +60,10 @@ class EventPage extends Component {
   }
 }
 
-export default EventPage
+const mapStateToProps = (state, ownProps) => {
+  return {
+    event: state.events[ownProps.match.params.Id]
+  }
+}
+
+export default connect(mapStateToProps)(EventPage)

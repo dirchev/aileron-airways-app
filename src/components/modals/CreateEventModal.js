06450f0d42ff39cf468b4001488f4ce98bf59cbc
dirchev <dirchev@camplight.net>
2018-03-12 11:02:23 +0000

    add create event modal

---
+++ src/components/modals/CreateEventModal.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+import moment from 'moment'
+import { connect } from 'react-redux'
+
+import Input from '../inputs/Input'
+import Textarea from '../inputs/Textarea'
+import eventActions from '../../action-creators/event'
+
+class CreateEventModal extends Component {
+  constructor () {
+    super()
+    this.state = {
+      title: '',
+      description: '',
+      eventDateTime: null,
+    }
+
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChange (field) {
+    return (value) => {
+      this.setState({[field]: value})
+    }
+  }
+
+  onSubmit (e) {
+    e.preventDefault()
+    this.props.createEvent({
+      TimelineId: this.props.timelineId,
+      Title: this.state.title,
+      Description: this.state.description,
+      EventDateTime: this.state.eventDateTime ? moment(this.state.eventDateTime).toISOString() : null
+    })
+    this.props.onClose()
+  }
+
+  render () {
+    return (
+      <div className="modal is-active">
+        <div className="modal-background"></div>
+        <div className="modal-content">
+          <div className="card">
+            <div className="card-header">
+              <div className="card-header-title">
+                <h2>Create Event</h2>
+              </div>
+            </div>
+            <div className="card-content">
+              <form onSubmit={this.onSubmit}>
+                <Input
+                  onChange={this.onChange('title')}
+                  value={this.state.title}
+                  label="Title"
+                  placeholder="Please enter event title..."
+                />
+                <Textarea
+                  onChange={this.onChange('description')}
+                  value={this.state.description}
+                  label="Description"
+                  placeholder="Please enter event description..."
+                />
+                <Input
+                  onChange={this.onChange('eventDateTime')}
+                  type='datetime-local'
+                  value={this.state.eventDateTime}
+                  label="Date and Time"
+                  placeholder="Please enter when the event happened..."
+                />
+                <div className="field is-grouped">
+                  <div className="control">
+                    <button className="button is-text" type="button" onClick={this.props.onClose}>Cancel</button>
+                  </div>
+                  <div className="control">
+                    <button className="button is-link" type="submit">Submit</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createEvent: (eventData) => dispatch(eventActions.create(eventData))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CreateEventModal)

e4b17c5073d028cbd8a60bb393a75fcfb260fa90
dirchev <dirchev@camplight.net>
2018-03-18 18:46:48 +0000

    implement pagination component

---
+++ src/components/Pagination.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Pagination extends Component {
+  constructor () {
+    super()
+    this.changePage = this.changePage.bind(this)
+  }
+
+  changePage (page) {
+    return (e) => {
+      e.preventDefault()
+      this.props.onPageChange(page)
+    }
+  }
+
+  render() {
+    // if there is only one page - no need to show anything
+    if (this.props.pages === 1) return null
+    return (
+      <nav className="pagination is-centered">
+        {
+          this.props.page !== 1
+          ? <a onClick={this.changePage(this.props.page - 1)} className="pagination-previous">Previous page</a>
+          : null
+        }
+        {
+          this.props.page !== this.props.pages
+          ? <a onClick={this.changePage(this.props.page + 1)} className="pagination-next">Next page</a>
+          : null
+        }
+        <ul className="pagination-list">
+          {
+            this.renderPageLinks()
+          }
+        </ul>
+      </nav>
+    )
+  }
+
+  renderPageLinks () {
+    var result = []
+    for (var i = 1; i <= this.props.pages; i++) {
+      result.push(
+        <li key={i}><a onClick={this.changePage(i)} className={`pagination-link ${i === this.props.page ? 'is-current' : ''}`} >{i}</a></li>
+      )
+    }
+    return result
+  }
+}
+
+Pagination.propTypes = {
+  page: PropTypes.number,
+  pages: PropTypes.number,
+  onPageChange: PropTypes.func,
+}
+export default Pagination

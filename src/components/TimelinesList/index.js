03bd6c2abda3eb3c77e4b37e5c550490025b529d
dirchev <dirchev@camplight.net>
2018-03-18 18:18:47 +0000

    update TimelinesList to gather timelines from state

--- src/components/TimelinesList/index.js
+++ src/components/TimelinesList/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Timeline from './TimelineListItem'
 import PropTypes from 'prop-types'
+import _ from 'lodash'
 
-/*
-   TimelinesList
-   gets an array of timelines and renders timeline items
-*/
 class TimelinesList extends Component {
   render() {
     return (
@@ -26,4 +24,11 @@ TimelinesList.propTypes = {
   timelines: PropTypes.array
 }
 
-export default TimelinesList
+const mapStateToProps = (state) => {
+  return {
+    // TODO get only starred timelines
+    timelines: _.chain(state.timelines).values().take(6).value(), // get 6 timelines
+  }
+}
+
+export default connect(mapStateToProps)(TimelinesList)

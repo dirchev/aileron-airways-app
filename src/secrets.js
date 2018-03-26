75ee2ba51f13d5dfe08a53dfcf8f95911867ff9c
dirchev <dirchev@camplight.net>
2018-03-26 12:50:59 +0100

    update secrets to use env variables

--- src/secrets.js
+++ src/secrets.js
@@ -1,7 +1,7 @@
 export default {
   'timelineSDK': {
-    endpoint: 'https://gcu.ideagen-development.com/',
-    TenantId: 'Team7',
-    AuthToken: 'AUTH_TOKEN'
+    endpoint: process.env.REACT_APP_ENDPOINT || 'https://timeline-api-dirchev.herokuapp.com/',
+    TenantId: process.env.REACT_APP_TENANT_ID,
+    AuthToken: process.env.REACT_APP_AUTH_TOKEN
   }
 }

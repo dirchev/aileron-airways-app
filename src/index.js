81bd5d42f24846bd93acbbc5344b21c01dccbc0f
dirchev <dirchev@camplight.net>
2018-04-01 15:36:28 +0100

    remove not needed import

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import datetime from 'react-datetime'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()

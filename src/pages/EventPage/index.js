6d640daf3be55d880c6982a8921440ef908254fe
dirchev <dirchev@camplight.net>
2018-04-01 23:38:49 +0100

    Event Page split
    * move event details to EventBox
    * implement LinkedEventsMap

---
+++ src/pages/EventPage/index.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link, Redirect } from 'react-router-dom'
+
+import eventActions from '../../action-creators/event'
+import EventOptionsButton from '../../components/option-buttons/EventOptionsButton'
+import Navigation from '../../components/Navigation'
+import EventBox from './EventBox'
+import LinkedEventsMap from './LinkedEventsMap'
+
+class EventPage extends Component {
+  constructor(props) {
+    super(props)
+    if (props.event) {
+      // record the timeline id
+      // we will use it when the event is deleted
+      this.timelineId = props.event.TimelineId
+    }
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
+  }
+
+  getNavigationItems() {
+    var backButton = (
+      <Link to={this.timelineId ? `/timeline/${this.timelineId}` : '/'}
+        className="navbar-item"
+        href=""
+        key="back-button">
+        <i className="fa fa-chevron-left mr-sm"></i>
+        Back
+      </Link>
+    )
+
+    return {
+      actionsLeft: [backButton],
+    }
+  }
+
+  handleTitleChange(newTitle) {
+    this.props.changeEventTitle({
+      ...this.props.event,
+      Title: newTitle
+    })
+  }
+
+  handleDescriptionChange(newDescription) {
+    this.props.changeEventDescription({
+      ...this.props.event,
+      Description: newDescription
+    })
+  }
+
+  render() {
+    if (!this.props.event) {
+      return (<Redirect to={this.timelineId ? `/timeline/${this.timelineId}` : '/'} />)
+    }
+    return (
+      <div className="mb-lg">
+        <Navigation {...this.getNavigationItems()} />
+        <EventOptionsButton event={this.props.event} deleteEvent={this.props.deleteEvent} />
+        <div className="container is-fluid">
+          <div className="columns mt-lg">
+            <div className="column is-one-quarter">
+              <LinkedEventsMap event={this.props.event} />
+            </div>
+            <div className="column">
+              <EventBox
+                event={this.props.event}
+                handleTitleChange={this.handleTitleChange}
+                handleDescriptionChange={this.handleDescriptionChange}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    event: state.events[ownProps.match.params.Id]
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    changeEventTitle: function (eventData) {
+      dispatch(eventActions.editTitle(eventData))
+    },
+    changeEventDescription: function (eventData) {
+      dispatch(eventActions.editDescription(eventData))
+    },
+    deleteEvent: (eventId, timelineId) => {
+      dispatch(eventActions.delete(eventId, timelineId))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventPage)

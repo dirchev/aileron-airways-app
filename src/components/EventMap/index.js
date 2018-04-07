72a2e218a5b2e80248640f2ecdbd5f1b2d53dab6
dirchev <dirchev@camplight.net>
2018-04-07 22:01:53 +0100

    show place label in EventMap

--- src/components/EventMap/index.js
+++ src/components/EventMap/index.js
@@ -4,15 +4,50 @@ import Map from './Map'
 import { Marker } from "react-google-maps"
 
 class EventMap extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      placeLabel: null
+    }
+  }
+
+  loadPlaceLabel (props) {
+    props = props || this.props
+    var geocoder = new window.google.maps.Geocoder()
+    var latLng = new window.google.maps.LatLng({lat: props.lat, lng: props.lng})
+
+    geocoder.geocode({
+      location: latLng,
+    }, (geocodeResult) => {
+      this.setState({placeLabel: geocodeResult[0].formatted_address})
+    })
+  }
+
+  componentDidMount () {
+    this.loadPlaceLabel()
+  }
+
+  componentWillReceiveProps (props) {
+    this.loadPlaceLabel(props)
+  }
+
   render () {
     var coords = {
       lat: this.props.lat,
       lng: this.props.lng
     }
     return (
-      <Map mapProps={{defaultCenter: coords}}>
-        <Marker position={coords} />
-      </Map>
+      <div>
+        {
+          this.state.placeLabel
+          ? (<div>{this.state.placeLabel}</div>)
+          : null
+        }
+        <Map mapProps={{defaultCenter: coords}}>
+          <Marker position={coords} />
+        </Map>
+      </div>
     )
   }
 }

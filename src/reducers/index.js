98b572d898b0e6de2cc167aa68c3dcb5477e6d7a
dirchev <dirchev@camplight.net>
2018-04-08 13:13:15 +0100

    add attachments related reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import timelinesReducer from './timelines'
 import eventsReducer from './events'
 import eventLinksReducer from './event-links'
 import uiReducer from './ui'
+import attachmentsReducer from './attachments'
 
 export default combineReducers({
   timelines: timelinesReducer,
   ui: uiReducer,
   events: eventsReducer,
-  eventLinks: eventLinksReducer
+  eventLinks: eventLinksReducer,
+  attachments: attachmentsReducer
 })

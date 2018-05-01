8fa1e4f296b051a5fa311b3ed59bbe3cd3de1567
dirchev <dimitar.mirchev96@gmail.com>
2018-05-01 02:58:43 +0100

    Update index.js
    1. Take logic from removed App.js
    2. Introduce react-router-redux
    3. Update usage of store.js

--- src/index.js
+++ src/index.js
@@ -2,25 +2,40 @@
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 import './build-index.css'
 import '../node_modules/react-datetime/css/react-datetime.css'
-
 // !CSS
 
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import HomePage from './pages/HomePage'
+import TimelinePage from './pages/TimelinePage'
+import EventPage from './pages/EventPage'
+import ModalsParent from './components/modals/ModalsParent'
+import { ConnectedRouter } from 'react-router-redux'
+import {BrowserRouter, Route} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 import { setStore } from './p2p-connection'
-
+import createHistory from 'history/createBrowserHistory'
 import { Provider } from 'react-redux'
-import store from './store'
+import getStore from './store'
+
+const history = createHistory()
+const store = getStore(history)
+setStore(store)
 
 var toRender = (
   <Provider store={store}>
-    <App loadDataOnStart />
+    <Fragment>
+      <ConnectedRouter history={history}>
+        <Fragment>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route exact path="/timeline/:Id" component={TimelinePage}></Route>
+          <Route exact path="/event/:Id" component={EventPage}></Route>
+        </Fragment>
+      </ConnectedRouter>
+      <ModalsParent />
+    </Fragment>
   </Provider>
 )
-
-setStore(store)
 
 ReactDOM.render(toRender, document.getElementById('root'))
 registerServiceWorker()

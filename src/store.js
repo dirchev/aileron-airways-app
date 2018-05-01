32460ac95347e8b18f4a412a078187227787aeaf
dirchev <dimitar.mirchev96@gmail.com>
2018-05-01 02:58:00 +0100

    update store.js to support parameters from outside

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import reducer from './reducers'
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
+import { routerMiddleware } from 'react-router-redux'
 
-var store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
-)
-
-export default store
+export default function getStore (history) {
+  const reactRouterMiddleware = routerMiddleware(history)
+  let store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk, reactRouterMiddleware)
+  )
+  return store
+}

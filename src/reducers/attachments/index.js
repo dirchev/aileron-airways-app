decf4674aae6002a7d13927b4d8e1af53c60fa7c
dirchev <dimitar.mirchev96@gmail.com>
2018-05-11 01:52:10 +0100

    refactor reducers

--- src/reducers/attachments/index.js
+++ src/reducers/attachments/index.js
@@ -1,63 +1,86 @@
 import _ from 'lodash'
 const defaultState = {}
 
-// This reducer keeps track of the event attachments
-// THE STATE IS AN OBJECT, and contains objects like
-// {
-//   '2': {
-//     TimelineEventId: '1',
-//     Id: '2'
-//     Title: 'Title',
-//     getURL: ''
-//   }
-// }
+/**
+ * Upsert Attachment
+ * Adds new attachment to the state or overrides currently existing one.
+ * The comparison is made by `attachmentData.Id` matching a key from the `state` object
+ *
+ * @param {Object} state current redux state
+ * @param {Object} attachmentData attachment to be added/edited
+ * @returns {Object} new version of the state
+ */
+const upsertAttachment = function (state, attachmentData) {
+  return {
+    ...state,
+    [attachmentData.Id]: {
+      ...state[attachmentData.Id],
+      ...attachmentData
+    }
+  }
+}
+
+/**
+ * Remove Attachment
+ * Removes an attachment from the state if exists.
+ * The comparison is made by `attachmentData.Id` matching a key from the `state` object
+ *
+ * @param {Object} state current redux state
+ * @param {Object} attachmentData attachment to be removed
+ * @returns {Object} new version of the state
+ */
+const removeAttachment = function (state, attachmentData) {
+  return _.omit(state, attachmentData.Id)
+}
+
+/**
+ * Upsert Attachments
+ * Goes through every attachment in the attachments array and upserts it to the state, using the
+ * upsertAttachment logic.
+ *
+ * @param {Object} state current redux state
+ * @param {Object} attachmentsArr attachments to be upserted
+ * @returns {Object} new version of the state
+ */
+const upsertAttachments = function (state, attachmentsArr) {
+  return attachmentsArr.reduce(upsertAttachment, state)
+}
+
+
 export default function eventLinksReducer(state = defaultState, action) {
   switch (action.type) {
     case 'SUCCESS_GET_EVENT_ATTACHMENTS':
-      var attachmentsObject = []
-      action.data.attachments.forEach(function (attachment) {
-        attachmentsObject[attachment.Id] = {
+      return upsertAttachments(
+        state,
+        action.data.attachments.map((attachment) => ({
           ...attachment,
           loading: false,
           synced: true
-        }
-      })
-      return {
-        ...state,
-        ...attachmentsObject
-      }
+        }))
+      )
     case 'START_CREATE_ATTACHMENT':
-      return {
-        ...state,
-        [action.data.Id]: {
-          ...action.data,
-          loading: true,
-          synced: false
-        }
-      }
+      return upsertAttachment(state, {
+        ...action.data,
+        loading: true,
+        synced: false
+      })
     case 'SYNC_ATTACHMENT':
     case 'SUCCESS_CREATE_ATTACHMENT':
-      return {
-        ...state,
-        [action.data.Id]: {
-          ...action.data,
-          loading: false,
-          synced: true
-        }
-      }
+      return upsertAttachment(state, {
+        ...action.data,
+        loading: false,
+        synced: true
+      })
     case 'ERROR_CREATE_ATTACHMENT':
     case 'ERROR_DELETE_ATTACHMENT':
-      return {
-        ...state,
-        [action.data.Id]: {
-          ...action.data,
-          loading: false,
-          synced: false,
-          error: action.error
-        }
-      }
+      return upsertAttachment(state, {
+        ...action.data,
+        loading: false,
+        synced: false,
+        error: action.error
+      })
     case 'SUCCESS_DELETE_ATTACHMENT':
-      return _.omit(state, action.data.Id)
+      return removeAttachment(state, action.data)
     default:
       return state
   }
--- src/reducers/timelines/index.js
+++ src/reducers/timelines/index.js
@@ -1,71 +1,100 @@
 import _ from 'lodash'
 const defaultState = {}
 
+/**
+ * Upsert Timeline
+ * Adds new timeline to the state or overrides currently existing one.
+ * The comparison is made by `timelineData.Id` matching a key from the `state` object
+ *
+ * @param {Object} state current redux state
+ * @param {Object} timelineData timeline to be added/edited
+ * @returns {Object} new version of the state
+ */
+const upsertTimeline = function (state, timelineData) {
+  return {
+    ...state,
+    [timelineData.Id]: {
+      ...state[timelineData.Id],
+      ...timelineData
+    }
+  }
+}
+
+/**
+ * Remove Timeline
+ * Removes a timeline from the state if exists.
+ * The comparison is made by `timelineData.Id` matching a key from the `state` object
+ *
+ * @param {Object} state current redux state
+ * @param {Object} timelineData timeline to be removed
+ * @returns {Object} new version of the state
+ */
+const removeTimeline = function (state, timelineData) {
+  return _.omit(state, timelineData.Id)
+}
+
+/**
+ * Upsert Timelines
+ * Goes through every timeline in the array and upserts it to the state, using the
+ * upsertTimeline logic.
+ *
+ * @param {Object} state current redux state
+ * @param {Object} timelinesArr timelines to be upserted
+ * @returns {Object} new version of the state
+ */
+const upsertTimelines = function (state, timelinesArr) {
+  return timelinesArr.reduce(upsertTimeline, state)
+}
+
 export default function timelinesReducer (state = defaultState, action) {
   switch(action.type) {
     case 'START_FETCH_EVENTS':
-      return {
-        ...state,
-        [action.data.TimelineId]: {
-          ...state[action.data.TimelineId],
-          loadingEvents: true
-        }
-      }
+      return upsertTimeline(state, {
+        ...action.data,
+        loadingEvents: true
+      })
     case 'SUCCESS_FETCH_EVENTS':
     case 'ERROR_FETCH_EVENTS':
-      return {
-        ...state,
-        [action.data.TimelineId]: {
-          ...state[action.data.TimelineId],
-          loadingEvents: false
-        }
-      }
+      return upsertTimeline(state, {
+        ...action.data,
+        loadingEvents: false
+      })
     case 'START_CREATE_TIMELINE':
     case 'START_EDIT_TIMELINE':
     case 'START_DELETE_TIMELINE':
-      return {
-        ...state,
-        [action.data.Id]: {
-          ...action.data,
-          loading: true,
-          synced: false
-        }
-      }
+      return upsertTimeline(state, {
+        ...action.data,
+        loading: true,
+        synced: false
+      })
     case 'SUCCESS_CREATE_TIMELINE':
     case 'SUCCESS_EDIT_TIMELINE':
     case 'SYNC_TIMELINE':
-      return {
-        ...state,
-        [action.data.Id]: {
-          ...action.data,
-          loading: false,
-          synced: true
-        }
-      }
+      return upsertTimeline(state, {
+        ...action.data,
+        loading: false,
+        synced: true
+      })
     case 'ERROR_EDIT_TIMELINE':
     case 'ERROR_CREATE_TIMELINE':
     case 'ERROR_DELETE_TIMELINE':
-      return {
-        ...state,
-        [action.data.Id]: {
-          ...state[action.data.Id],
-          loading: false,
-          synced: false,
-          error: action.error
-        }
-      }
-    case 'SUCCESS_DELETE_TIMELINE':
-      return _.omit(state, action.data.Id)
-
-    case 'SUCCESS_FETCH_TIMELINES':
-      var newState = {}
-      action.data.forEach(function (timeline) {
-        newState[timeline.Id] = {loading: false, synced: true, ...timeline}
+      return upsertTimeline(state, {
+        ...action.data,
+        loading: false,
+        synced: false,
+        error: action.error
       })
-      return {
-        ...state,
-        ...newState,
-      }
+    case 'SUCCESS_DELETE_TIMELINE':
+      return removeTimeline(state, action.data)
+    case 'SUCCESS_FETCH_TIMELINES':
+      return upsertTimelines(
+        state,
+        action.data.map((item) => ({
+          ...item,
+          loading: false,
+          synced: true
+        }))
+      )
     default:
       return state
   }
